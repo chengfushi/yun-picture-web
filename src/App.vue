368c10510b1bdbe3281b6fbda40b1056f9b1afa8
chengfushi <303623518@qq.com>
2025-06-20 21:59:51 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from './layous/BasicLayout.vue'
+import {userLoginUserStore} from '@/stores/userLoginUserStore'
+
+const loginUserStore = userLoginUserStore()
+loginUserStore.fetchLoginUser()
+</script>
+<style>
+#app {
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="homePage">
+    <h1>欢迎来到智图运链云图库，项目应用场景广泛，可作为表情包网站、设计素材网站、壁纸网站、个人云盘、企业活动相册等</h1>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
