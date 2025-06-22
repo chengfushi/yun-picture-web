7e2cc20023d7e5762eb59bb7199503e3908475c9
chengfushi <303623518@qq.com>
2025-06-22 13:40:40 +0800

    登录与注册页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
+import UserLoginPage from '@/pages/user/UserLoginPage.vue'
+import UserRegisterPage from '@/pages/user/UserRegisterPage.vue'
+import UserManagerPage from '@/pages/admin/UserManagerPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +13,20 @@ const router = createRouter({
       component: HomePage,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/user/login',
+      name: 'login',
+      component: UserLoginPage,
     },
+    {
+      path: '/user/register',
+      name: 'register',
+      component: UserRegisterPage,
+    },
+    {
+      path: '/admin/user',
+      name: 'admin-user',
+      component: UserManagerPage,
+    }
   ],
 })
 
